50cb0b04a7b36d1104cb7af2a3e106b3ae1e2f68
bvsrajeev <bvsrajeev@gmail.com>
2017-08-25 22:55:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,11 +4,11 @@ var img = document.getElementById('madi');
 var marginLeft = 0;
 
 function moveRight(){
-    marginLeft = marginLeft + 10;
+    marginLeft = marginLeft + 05;
     img.style.marginLeft = marginLeft+'px';
 }
 
 img.onclick = function(){
     //img.style.marginLeft = '100px';
-    var interval = setInterval(moveRight,100);
+    var interval = setInterval(moveRight,50);
 };
